fda105ccbebfac5cf6fdaf08fb818b04ffcf110a
Michael Theis (msg) <michael.theis@msg.group>
2021-11-25 18:51:37 +0100

    fixed outputs in module database/elasticsearch/kubernetes

--- modules/database/elasticsearch/kubernetes/outputs.tf
+++ modules/database/elasticsearch/kubernetes/outputs.tf
@@ -1,6 +1,6 @@
 output elasticsearch_service_name {
   description = "Name of the Kubernetes service referring to this elasticsearch cluster"
-  value = "${var.elasticsearch_cluster_name}.${var.kubernetes_namespace_name}"
+  value = "${var.elasticsearch_cluster_name}-master.${var.kubernetes_namespace_name}"
 }
 
 output elasticsearch_service_port {
