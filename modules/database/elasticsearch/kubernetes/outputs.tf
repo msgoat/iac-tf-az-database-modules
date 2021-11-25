8e9cf54e92ef7e63723449821e88c9aa06bd07e8
Michael Theis (msg) <michael.theis@msg.group>
2021-11-25 17:33:53 +0100

    added missing outputs to module database/elasticsearch/kubernetes

---
+++ modules/database/elasticsearch/kubernetes/outputs.tf
@@ -0,0 +1,9 @@
+output elasticsearch_service_name {
+  description = "Name of the Kubernetes service referring to this elasticsearch cluster"
+  value = "${var.elasticsearch_cluster_name}.${var.kubernetes_namespace_name}"
+}
+
+output elasticsearch_service_port {
+  description = "Port of the Kubernetes service referring to this elasticsearch cluster"
+  value = "9200"
+}
