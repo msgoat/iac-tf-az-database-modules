1d41dd99cae5c75b2811fa3099d5f31332cd8971
Michael Theis (msg) <michael.theis@msg.group>
2023-01-17 13:03:37 +0100

    database/elasticsearch/kubernetes: fixed some glitches with kubernetes provider configuration

--- modules/database/elasticsearch/kubernetes/kubernetes_provider.tf
+++ modules/database/elasticsearch/kubernetes/kubernetes_provider.tf
@@ -1,8 +1,8 @@
 provider kubernetes {
-    host                   = data.azurerm_kubernetes_cluster.cluster.kube_config.0.host
-    username               = data.azurerm_kubernetes_cluster.cluster.kube_config.0.username
-    password               = data.azurerm_kubernetes_cluster.cluster.kube_config.0.password
-    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate)
-    client_key             = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.client_key)
-    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)
+    host                   = local.aks_host
+    username               = local.aks_username
+    password               = local.aks_password
+    client_certificate     = local.aks_client_certificate
+    client_key             = local.aks_client_key
+    cluster_ca_certificate = local.aks_cluster_ca_certificate
 }
